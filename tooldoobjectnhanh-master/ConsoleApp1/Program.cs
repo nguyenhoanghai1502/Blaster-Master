687f7b7ec37b6380bf173dc7161d26ff1cb74f81
Nguyễn Hải <you@example.com>
2020-12-03 11:04:18 +0700

    Sửa file object, dòng 1-47

--- tooldoobjectnhanh-master/ConsoleApp1/Program.cs
+++ tooldoobjectnhanh-master/ConsoleApp1/Program.cs
@@ -7,19 +7,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int x1, y1, x2, y2;
-            Console.WriteLine("Nhap so cot bat dau: ");
-            x1 = Convert.ToInt32(Console.ReadLine())+1;
-            Console.WriteLine("Nhap so cot ket thuc: ");
-            x2 = Convert.ToInt32(Console.ReadLine())+1;
-            Console.WriteLine("Nhap so hang bat dau: ");
-            y1= Convert.ToInt32(Console.ReadLine())+1;
-            Console.WriteLine("Nhap so hang ket thuc: ");
-            y2 = Convert.ToInt32(Console.ReadLine())+1;
-            Console.Write("1" + " " + "0" + " " + x1 + " " + y1 + " " + (x2 - x1) + " " + (y2 - y1) + " " + "0 0 0 0" + " ");
-            InRaManHinh(x1/16, y1/16, x2/16, y2/16);
-
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                int x1, y1, x2, y2;
+                Console.WriteLine("Nhap so cot bat dau: ");
+                x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nhap so cot ket thuc: ");
+                x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nhap so hang bat dau: ");
+                y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Nhap so hang ket thuc: ");
+                y2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write(n+" "+"1" + " " + x1 + " " + y1 + " " + (x2-x1) + " " + (y2-y1) + " ");
+                InRaManHinh(x1 / 64, y1 / 64, x2 / 64, y2 / 64);
+                Console.WriteLine();
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n != -1);
         }
         static void InRaManHinh(int x1, int y1, int x2, int y2)
         {
